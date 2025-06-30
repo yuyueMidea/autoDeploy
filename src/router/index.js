f5d4a15fb30cbde371ee46084f10a247fac177c9
苍穹勇敢的心 <18825237023@163.com>
2025-06-30 23:53:26 +0800

    路由改动

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '../components/Home.vue'
+// import Home from '../components/Home.vue'
 // import Contact from '../components/Contact.vue'
 const router = createRouter(
     {
         history: createWebHistory(),
         routes: [
-            {path: '/', component: Home},
+            { path: '/', name:'home', component: () => import('../components/Home.vue')},
             { path: '/eggs/:eggType', name: 'eggs', component: () => import('../components/Contact.vue') },
             { path: '/eggs', redirect: '/eggs/chicken-egg' },
             { path: '/:pathMatch(.*)*', component: () => import('../components/NotFound.vue') }
