86b664721d24db12c768b3e89dea3fbcda63171a
苍穹勇敢的心 <18825237023@163.com>
2025-06-30 23:52:46 +0800

    修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">主页</router-link> | 
-    <router-link v-for="dataEgg in dataEggs" :key="dataEgg.id" :title="dataEgg.type"
-      :to="{name:'eggs', params:{eggType:dataEgg.type}}">{{dataEgg.name}} | </router-link>
-      <button @click="backward">退后</button>
-      <button @click="forward">前进</button>
-  </nav>
-  <router-view></router-view>
+  <div id="app_wrapper">
+    <nav class="app_header">
+      <router-link to="/">主页</router-link> | 
+      <router-link v-for="dataEgg in dataEggs" :key="dataEgg.id" :title="dataEgg.type"
+        :to="{name:'eggs', params:{eggType:dataEgg.type}}">{{dataEgg.name}} | </router-link>
+        <button @click="backward">退后</button>
+        <button @click="forward">前进</button>
+    </nav>
+    <div class="app_main">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,3 +32,41 @@ export default {
   }
 }
 </script>
+
+
+<style>
+#app_wrapper {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.app_header {
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  background: #ccc;
+}
+.app_header a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 4px;
+}
+
+.app_header a.router-link-exact-active {
+  color: white;
+  background: #42b983;
+}
+
+.app_main {
+  width: 100%;
+  height: calc(100vh - 60px);
+  overflow-y: auto;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 import router from './router'
-import { groupBy } from 'lodash'
-console.log('groupBy: ', groupBy)
 createApp(App).use(router).mount('#app')
