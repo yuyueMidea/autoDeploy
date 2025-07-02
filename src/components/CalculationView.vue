d989b4ca64b8646492d7b05ab8d43407fe241cf9
苍穹勇敢的心 <18825237023@163.com>
2025-07-02 16:12:08 +0800

    webworker计算界面，添加按钮点击记录，和耗时统计信息

--- src/components/CalculationView.vue
+++ src/components/CalculationView.vue
@@ -2,18 +2,18 @@
     <div class="calculation-view">
       <h2>耗时计算界面</h2>
       
-      <div v-if="!isCalculating && !result">
+      <div class="border p-5 m-5">
         <!-- <button @click="startCalculation">开始计算</button> -->
-        <input type="text" v-model="maxCount" placeholder="please input timeCount" />
-        <button v-permission="'admin'" class="btn" @click="startCalculation">开始计算(管理员权限)</button>
+        <input type="text" v-model="maxCount" placeholder="please input timeCount" class="p-5 m-5" />
+        <button v-track.click="'calcBtn'" v-permission="'admin'" class="btn" @click="startCalculation">开始计算(管理员权限)</button>
       </div>
       
-      <div v-if="isCalculating">
+      <div v-if="isCalculating" class="border p-5 m-5">
         <p>计算进度: {{ progress }}%</p>
         <p>计算中... 可以切换到其他界面，计算不会中断</p>
       </div>
       
-      <div v-if="result">
+      <div v-if="result" class="border p-5 m-5">
         <h3>计算结果: {{ result }}</h3>
         <p>计算完成时间: {{ completionTime }}</p>
       </div>
@@ -26,7 +26,7 @@ import { ref, onMounted, onUnmounted, onActivated, onDeactivated } from 'vue';
 import Worker from '@/calculator.worker.js?worker';
 defineOptions({ name: 'calcPage' });
 
-
+const startPerf = ref(null);
 
 const worker = ref(null);
 const isCalculating = ref(false);
@@ -47,6 +47,9 @@ const initWorker = () => {
       result.value = res.toFixed(4);
       completionTime.value = new Date().toLocaleTimeString();
       isCalculating.value = false;
+      // 标记以下结束时间
+      const endPerf = performance.now();
+      console.log(`maxcnt_: ${maxCount.value}, 耗时：${endPerf - startPerf.value} 微秒`); // 高精度
     }
   };
 };
@@ -54,6 +57,9 @@ const initWorker = () => {
 const maxCount = ref(500000000);
 // 开始计算
 const startCalculation = () => {
+  // 标记开始时间
+  startPerf.value = performance.now();
+  console.log( `maxcnt_: ${maxCount.value}, start_: ${startPerf.value}` )
   if (!worker.value) initWorker();
   
   isCalculating.value = true;
