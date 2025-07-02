26c76ca74cafabce33d8bba1efca8db5b2f4a37e
苍穹勇敢的心 <18825237023@163.com>
2025-07-02 16:13:22 +0800

    根据状态勾选，确定是否添加点击记录

--- src/utils/trackClick.js
+++ src/utils/trackClick.js
@@ -1,19 +1,23 @@
+import { useAppStore } from "../store/useCommonStore";
 import { logEvtToLocalStorage } from "./log";
 
 export default {
     mounted(el, binding) {
+        const app1store = useAppStore();
         const eventName = binding.arg || 'click';
         const trackData = binding.value;
 
         el.addEventListener(eventName, (e) =>{
-            logEvtToLocalStorage({
-                timestamp: new Date().toISOString(),
-                element: el.tagName.toLowerCase(),
-                id: el.id || null,
-                class: el.className || null,
-                text: el.textContent.trim().substring(0, 50),
-                ...(typeof trackData === 'object' ? trackData : { value: trackData })
-            })
+            if(app1store.logClickFlag) {
+                logEvtToLocalStorage({
+                    timestamp: new Date().toISOString(),
+                    element: el.tagName.toLowerCase(),
+                    id: el.id || null,
+                    class: el.className || null,
+                    text: el.textContent.trim().substring(0, 50),
+                    ...(typeof trackData === 'object' ? trackData : { value: trackData })
+                })
+            }
         })
     }
 }
