ae91f3dbe28121274ddf14328f1d0d0ec94f5860
苍穹勇敢的心 <18825237023@163.com>
2025-07-05 11:17:49 +0800

    main.js全局注册列表组件

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import router from './router'
 import { createPinia } from 'pinia'
 import trackClick from './utils/trackClick'
 import { permissionDirective, permissionMixin, setPermissions } from './utils/permission'
+import GlobalList from './components/GlobalList.vue'
 // createApp(App).use(router).mount('#app')
-
 
 const app = createApp(App);
 app.use(createPinia());
 
+// 全局注册列表组件
+app.component('CommonList', GlobalList)
 // 注册点击追踪指令
 app.directive('track', trackClick);
 // 注册全局权限指令
