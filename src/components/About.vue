fbc754bd77249beb475c373aeeca8295d5d387dc
苍穹勇敢的心 <18825237023@163.com>
2025-07-05 11:18:42 +0800

    about页面引入使用通用列表，

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,12 +1,39 @@
 <template>
-    <div>
-      <h2>关于我们</h2>
-      <p v-for="item in 22" key="item">这是关于页面内容 {{ item }}</p>
-    </div>
-  </template>
+  <div>
+    <h2>list-items
+    </h2>
+    <CommonList :items="users">
+      <template #header>
+        <p>共 {{ users.length }} 个</p>
+      </template>
+      <!-- Footer 插槽内容 -->
+       <template #footer>
+        <button @click="loadMore">加载更多</button>
+       </template>
+    </CommonList>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: 'About'
-  }
-  </script>
+<script setup>
+import { ref } from 'vue';
+// 生成随机ID的方法，入参是位数，比如16位ID
+const generateRandomString = (length) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return Array.from(crypto.getRandomValues(new Uint8Array(length)))
+    .map(byte => chars[byte % chars.length])
+    .join('');
+}
+// 初始化user数据列表，生成几条
+const users = ref([]);
+for(let i=0;i<5;i++) {
+  users.value.push({ id: generateRandomString(16) })
+}
+
+const loadMore = () => {
+  // 加载更多数据逻辑
+  const ulen = users.value.length
+  console.log('加载更多数据', ulen)
+  users.value.push({ id: generateRandomString(16) })
+}
+
+</script>
